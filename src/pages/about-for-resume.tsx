3495e1674577c4c5475f91535a09f10e61a60d79
HunnyJ434 <fhunny@lakeheadu.ca>
2024-02-07 12:53:28 -0500

    Update 1.5

---
+++ src/pages/about-for-resume.tsx
@@ -0,0 +1,11 @@
+const Page =  () => {
+
+    return (
+      
+      <div>
+        <h1>Build by Hunny J. Please feel free to browse the website.</h1>
+      </div>
+    );
+  }
+  
+  export default Page
